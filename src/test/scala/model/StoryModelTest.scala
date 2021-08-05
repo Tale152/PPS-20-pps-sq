6fbca9745e005a63b9d7a92d3a4403f5cefd9edb
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-08-05 11:44:51 +0200

    Tested Story model

--- src/test/scala/model/StoryModelTest.scala
+++ src/test/scala/model/StoryModelTest.scala
@@ -1,14 +1,26 @@
 package model
 
 import model.characters.Player
+import model.nodes.StoryNode
 import org.scalatest.{FlatSpec, Matchers}
 
 class StoryModelTest extends FlatSpec with Matchers {
 
   val mainPlayer: Player = Player("Jonathan")
-  val storyModel: StoryModel = StoryModel(mainPlayer)
+  val storyNode: StoryNode = StoryNode(0, "narrative", Set.empty)
+  val storyNode1: StoryNode = StoryNode(1, "narrative1", Set.empty)
+  val storyModel: StoryModel = StoryModelImpl(mainPlayer, storyNode)
 
   "The story model" should "know who is the player" in {
     storyModel.player shouldEqual mainPlayer
+  }
+
+  it should "know which is the current story node" in {
+    storyModel.currentStoryNode shouldEqual storyNode
+  }
+
+  it should "update the current story node" in {
+    storyModel.currentStoryNode = storyNode1
+    storyModel.currentStoryNode shouldEqual storyNode1
   }
 }
