db3280b94a733a65c70e8bb752c6bc6f70c4d8f3
Piero Sanchi <piero.sanchi@studio.unibo.it>
2021-08-09 17:23:43 +0200

    created and tested health class

---
+++ src/main/scala/model/characters/properties/Health.scala
@@ -0,0 +1,36 @@
+package model.characters.properties
+
+object Health {
+
+  /**
+   * Trait that represents the Health of a Character.
+   */
+  sealed trait Health {
+    val maxPS: Int
+
+    def currentPS: Int
+
+    def currentPS_=(newCurrentPS: Int): Unit
+  }
+
+  def apply(maxPS: Int): Health = new HealthImpl(maxPS: Int)
+
+  /**
+   * the implementation of Health
+   *
+   * @param maxPS the max amount of total PS reachable
+   */
+  private class HealthImpl(override val maxPS: Int) extends Health {
+    require(maxPS != 0)
+    private var _currentPS = maxPS
+
+    override def currentPS: Int = _currentPS
+
+    override def currentPS_=(newCurrentPS: Int): Unit = if (newCurrentPS <= maxPS) {
+      _currentPS = newCurrentPS
+    } else {
+      _currentPS = maxPS
+    }
+  }
+
+}
