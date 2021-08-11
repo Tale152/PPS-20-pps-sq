c18bcab9215a5ecbaa3ddfd7b9bfc7e568ff2658
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-08-11 11:23:29 +0200

    Implemented and tested Stat

--- src/main/scala/model/characters/properties/StatName.scala
+++ src/main/scala/model/characters/properties/StatName.scala
@@ -2,11 +2,15 @@ package model.characters.properties
 
 object StatName {
 
+  /**
+   * Trait used by [[model.characters.properties.Stat]] and [[model.characters.properties.StatModifier]] to
+   * specify the name of the property they referred to.
+   */
   trait StatName
 
   case object Strength extends StatName
   case object Speed extends StatName
-  case object Observation extends StatName
+  case object Defence extends StatName
   case object Dexterity extends StatName
   case object Constitution extends StatName
   case object Intelligence extends StatName
---
+++ src/test/scala/model/characters/properties/StatTest.scala
@@ -0,0 +1,31 @@
+package model.characters.properties
+
+import model.characters.properties.StatName.StatName
+import org.scalatest.{FlatSpec, Matchers}
+
+class StatTest extends FlatSpec with Matchers{
+
+  val strengthValue: Int = 10
+
+  var undefinedStatName: StatName = _
+
+  val strengthStat: Stat = Stat(strengthValue, StatName.Strength)
+
+  "The stat" should "have a value" in {
+    strengthStat.value() shouldEqual strengthValue
+  }
+
+  it should "have a stat name it is referred to" in {
+    strengthStat.statName() shouldEqual StatName.Strength
+  }
+
+  it should "not match other stat name" in {
+    strengthStat.statName() should not equal StatName.Wisdom
+  }
+
+  "The stat name" should "not be undefined" in {
+    intercept[IllegalArgumentException] {
+      Stat(strengthValue, undefinedStatName)
+    }
+  }
+}
