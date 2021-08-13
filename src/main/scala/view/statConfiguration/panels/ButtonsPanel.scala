dd1de5398e77f712145a64f6ce550f40bbfa317d
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-13 09:40:27 +0200

    documented ButtonsPanel

--- src/main/scala/view/statConfiguration/panels/ButtonsPanel.scala
+++ src/main/scala/view/statConfiguration/panels/ButtonsPanel.scala
@@ -5,6 +5,12 @@ import javax.swing.{JButton, JPanel}
 
 object ButtonsPanel {
 
+  /**
+   * Panel contained in [[view.statConfiguration.StatConfigurationView]]; renders two button:
+   * one to confirm and one to go back
+   * @param onBack strategy to be applied on back button click
+   * @param onConfirm strategy to be applied on confirm button click
+   */
   class ButtonsPanel(private val onBack: Unit => Unit, private val onConfirm: Unit => Unit) extends JPanel{
     this.setOpaque(false)
     private val backButton: JButton = new JButton("Back")
