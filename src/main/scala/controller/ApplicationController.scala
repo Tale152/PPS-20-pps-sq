3943aa73928972ca292756a58d0bbdcfc70c9f24
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-13 10:01:21 +0200

    documented ApplicationController

--- src/main/scala/controller/ApplicationController.scala
+++ src/main/scala/controller/ApplicationController.scala
@@ -4,8 +4,19 @@ import controller.util.DirectoryInitializer.initializeGameFolderStructure
 import controller.util.ResourceName
 import model.nodes.util.StoryNodeSerializer.deserializeStory
 
+/**
+ * The Application Controller is the Main Controller of the application.
+ * It's the entrypoint and controls the main menu.
+ */
 sealed trait ApplicationController extends Controller {
 
+  /**
+   * Load a story starting a new game. Once the story is loaded the control will be granted to the
+   * StatConfigurationController in order to allow the user to choose his stats.
+   * @param storyUri the uri where the story that will be loaded is located
+   * @see [[StatConfigurationController]]
+   * @see [[model.characters.properties.stats.Stat]]
+   */
   def loadStoryNewGame(storyUri: String): Unit
 }
 
