324ff2150ac67804e45d71750a6eecc701da5776
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-13 09:43:18 +0200

    documented RemainingPointsPanel

--- src/main/scala/view/statConfiguration/panels/RemainingPointsPanel.scala
+++ src/main/scala/view/statConfiguration/panels/RemainingPointsPanel.scala
@@ -5,6 +5,11 @@ import javax.swing.{JLabel, JPanel}
 
 object RemainingPointsPanel {
 
+  /**
+   * Panel contained in [[view.statConfiguration.StatConfigurationView]]; renders a label containing the number
+   * of remaining points. If said number has reached zero it will be printed in red, green otherwise.
+   * @param remainingPoints number of remaining points to render
+   */
   class RemainingPointsPanel(val remainingPoints: Int) extends JPanel {
     this.setOpaque(false)
     private val textLabel: JLabel = new JLabel("Remaining points:")
