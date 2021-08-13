7c62e83a9c2d65a4f5915154d5be911249fc3a66
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-13 10:14:13 +0200

    updated SubController doc

--- src/main/scala/controller/game/subcontroller/SubController.scala
+++ src/main/scala/controller/game/subcontroller/SubController.scala
@@ -4,9 +4,11 @@ import controller.Controller
 import controller.game.GameMasterController
 
 /**
- * A SubController is a sub-component of the MasterController.
- * SubControllers are coordinated by the MasterController using the
+ * A SubController is a sub-component of the GameMasterController.
+ * SubControllers are coordinated by the GameMasterController using the
  * [[GameMasterController#executeOperation(controller.OperationType)]] method,
- * which delegates the job to the correct SubController specifying the corresponding [[controller.OperationType]].
+ * which delegates the job to the correct SubController specifying the corresponding OperationType.
+ * @see [[controller.game.GameMasterController]]
+ * @see [[controller.game.OperationType]]
  */
 trait SubController extends Controller
