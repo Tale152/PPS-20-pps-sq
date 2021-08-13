7b6302549aadc88794d3874134bf0d2a2c155f67
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-13 09:48:07 +0200

    documented StatEditPanel

--- src/main/scala/view/statConfiguration/panels/StatEditPanel.scala
+++ src/main/scala/view/statConfiguration/panels/StatEditPanel.scala
@@ -8,6 +8,17 @@ import javax.swing.{JButton, JLabel, JPanel}
 
 object StatEditPanel {
 
+  /**
+   * Panel contained in [[view.statConfiguration.StatConfigurationView]]; renders a form containing the stat name
+   * and value, alongside a minus button and a plus button.
+   * When stat value equals one the minus button will be disabled; similarly when remaining points equals zero the plus
+   * button will be disabled.
+   * @param stat the stat which name and value will be rendered
+   * @param remainingPoints the number of remaining points used for button enable/disable
+   * @param onMinus strategy to apply on minus button click
+   * @param onPlus strategy to apply on plus button click
+   * @see [[model.characters.properties.stats.Stat]]
+   */
   class StatEditPanel(private val stat: Stat,
                       private val remainingPoints: Int,
                       private val onMinus: Unit => Unit,
