2a8eba9732b201396bb9752e81f8b5a9f237a3a5
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-14 12:10:26 +0200

    added StatStatusController to SubControllersContainer

--- src/main/scala/controller/game/SubControllersContainer.scala
+++ src/main/scala/controller/game/SubControllersContainer.scala
@@ -1,6 +1,6 @@
 package controller.game
 
-import controller.game.subcontroller.StoryController
+import controller.game.subcontroller.{StatStatusController, StoryController}
 import model.StoryModel
 
 /**
@@ -13,6 +13,11 @@ sealed trait SubControllersContainer {
    * @return the [[controller.game.subcontroller.StoryController]] instance in the current game.
    */
   def storyController: StoryController
+
+  /**
+   * @return the [[controller.game.subcontroller.StatStatusController]] instance in the current game.
+   */
+  def statStatusController: StatStatusController
 }
 
 object SubControllersContainer {
@@ -21,8 +26,11 @@ object SubControllersContainer {
                                             storyModel: StoryModel) extends SubControllersContainer {
 
     private val _storyController: StoryController = StoryController(gameMasterController, storyModel)
+    private val _statStatusController: StatStatusController = StatStatusController(gameMasterController, storyModel)
 
     override def storyController: StoryController = _storyController
+
+    override def statStatusController: StatStatusController = _statStatusController
   }
 
   def apply(gameMasterController: GameMasterController, storyModel: StoryModel): SubControllersContainer =
