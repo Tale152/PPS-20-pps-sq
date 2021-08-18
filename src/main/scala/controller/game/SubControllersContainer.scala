5e7c430780e053419b3aaaba228ac7495005ebb0
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-18 21:11:14 +0200

    added HistoryController into SubControllersContainer

--- src/main/scala/controller/game/SubControllersContainer.scala
+++ src/main/scala/controller/game/SubControllersContainer.scala
@@ -1,6 +1,6 @@
 package controller.game
 
-import controller.game.subcontroller.{PlayerInfoController, StoryController}
+import controller.game.subcontroller.{HistoryController, PlayerInfoController, StoryController}
 import model.StoryModel
 
 /**
@@ -18,6 +18,11 @@ sealed trait SubControllersContainer {
    * @return the [[controller.game.subcontroller.PlayerInfoController]] instance in the current game.
    */
   def statStatusController: PlayerInfoController
+
+  /**
+   * @return the [[controller.game.subcontroller.HistoryController]] instance in the current game.
+   */
+  def historyController: HistoryController
 }
 
 object SubControllersContainer {
@@ -27,10 +32,13 @@ object SubControllersContainer {
 
     private val _storyController: StoryController = StoryController(gameMasterController, storyModel)
     private val _statStatusController: PlayerInfoController = PlayerInfoController(gameMasterController, storyModel)
+    private val _historyController: HistoryController = HistoryController(gameMasterController, storyModel)
 
     override def storyController: StoryController = _storyController
 
     override def statStatusController: PlayerInfoController = _statStatusController
+
+    override def historyController: HistoryController = _historyController
   }
 
   def apply(gameMasterController: GameMasterController, storyModel: StoryModel): SubControllersContainer =
