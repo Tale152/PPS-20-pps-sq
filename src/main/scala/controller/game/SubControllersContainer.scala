4475c4755233c0baa3179fabcd86b3e3c6bca943
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-23 16:43:34 +0200

    integrated ProgressSaverController in SubControllersContainer

--- src/main/scala/controller/game/SubControllersContainer.scala
+++ src/main/scala/controller/game/SubControllersContainer.scala
@@ -1,6 +1,6 @@
 package controller.game
 
-import controller.game.subcontroller.{HistoryController, PlayerInfoController, StoryController}
+import controller.game.subcontroller.{HistoryController, PlayerInfoController, ProgressSaverController, StoryController}
 import model.StoryModel
 
 /**
@@ -23,6 +23,11 @@ sealed trait SubControllersContainer {
    * @return the [[controller.game.subcontroller.HistoryController]] instance in the current game.
    */
   def historyController: HistoryController
+
+  /**
+   * @return the [[controller.game.subcontroller.ProgressSaverController]] instance in the current game.
+   */
+  def progressSaverController: ProgressSaverController
 }
 
 object SubControllersContainer {
@@ -30,15 +35,18 @@ object SubControllersContainer {
   private class SubControllersContainerImpl(gameMasterController: GameMasterController,
                                             storyModel: StoryModel) extends SubControllersContainer {
 
-    private val _storyController: StoryController = StoryController(gameMasterController, storyModel)
-    private val _statStatusController: PlayerInfoController = PlayerInfoController(gameMasterController, storyModel)
-    private val _historyController: HistoryController = HistoryController(gameMasterController, storyModel)
+    private val story: StoryController = StoryController(gameMasterController, storyModel)
+    private val statStatus: PlayerInfoController = PlayerInfoController(gameMasterController, storyModel)
+    private val history: HistoryController = HistoryController(gameMasterController, storyModel)
+    private val progressSaver: ProgressSaverController = ProgressSaverController(gameMasterController, storyModel)
 
-    override def storyController: StoryController = _storyController
+    override def storyController: StoryController = story
 
-    override def statStatusController: PlayerInfoController = _statStatusController
+    override def statStatusController: PlayerInfoController = statStatus
 
-    override def historyController: HistoryController = _historyController
+    override def historyController: HistoryController = history
+
+    override def progressSaverController: ProgressSaverController = progressSaver
   }
 
   def apply(gameMasterController: GameMasterController, storyModel: StoryModel): SubControllersContainer =
