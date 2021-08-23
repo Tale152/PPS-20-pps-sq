a9e1333af58176455d799a2c0d6730b036669fa8
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-08-23 16:57:37 +0200

    Created AbstractItem

--- src/main/scala/model/items/Item.scala
+++ src/main/scala/model/items/Item.scala
@@ -8,3 +8,25 @@ trait Item {
   val description: String
   def use(character: Character): Unit
 }
+
+/**
+ * The abstract class that describe an item.
+ * @param name the item name.
+ * @param description the item description.
+ */
+abstract class AbstractItem(override val name: String,
+                            override val description: String) extends Item {
+
+  /**
+   * Template method that use [[model.items.AbstractItem#applyEffect(java.lang.Character)]]
+   * and [[model.items.AbstractItem#postEffect(java.lang.Character)]].
+   * @param character, the target of the item effect.
+   */
+  override def use(character: Character): Unit = {
+    applyEffect(character)
+    postEffect(character)
+  }
+
+  abstract def applyEffect(character: Character): Unit
+  abstract def postEffect(character: Character): Unit
+}
