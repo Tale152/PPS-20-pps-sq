d154a6e2fee391360770d5f2bc10947f56d5b1b2
Piero Sanchi <piero.sanchi@studio.unibo.it>
2021-08-23 16:59:34 +0200

    added test to enemy on CharacterTest

--- src/test/scala/model/characters/CharacterTest.scala
+++ src/test/scala/model/characters/CharacterTest.scala
@@ -6,17 +6,29 @@ import org.scalatest.{FlatSpec, Matchers}
 class CharacterTest extends FlatSpec with Matchers {
 
   val maxPS: Int = 100
-  val wrongMaxPS: Int = 100
+  val wrongMaxPS: Int = -3
   val stats: Set[Stat] = Set(Stat(1, StatName.Speed))
   val mainPlayer: Player = Player("Jonathan", maxPS, stats)
+  val easyEnemy: Enemy = Enemy("Yoshikage Kira", maxPS, stats)
 
   "The player" should "have name Jonathan" in {
     mainPlayer.name shouldEqual "Jonathan"
   }
 
+  "The easy enemy" should "have name Yoshikage Kira" in {
+    easyEnemy.name shouldEqual "Yoshikage Kira"
+  }
+
   it should "throw IllegalArgumentException if the player name is left empty" in {
     intercept[IllegalArgumentException] {
       Player("", maxPS, stats)
+    }
+  }
+
+
+  it should "throw IllegalArgumentException if the enemy name is left empty" in {
+    intercept[IllegalArgumentException] {
+      Enemy("", maxPS, stats)
     }
   }
 
@@ -26,9 +38,15 @@ class CharacterTest extends FlatSpec with Matchers {
     }
   }
 
+  it should "throw IllegalArgumentException if maxPS is negative" in {
+    intercept[IllegalArgumentException] {
+      Player("Jolyne", wrongMaxPS, stats)
+    }
+  }
+
   it should "throw IllegalArgumentException if stats are empty" in {
     intercept[IllegalArgumentException] {
-      Player("Joseph",maxPS, Set())
+      Player("Joseph", maxPS, Set())
     }
   }
 
