18b3d3b48aa7db7dcd3396a3a7bd6c31cd4cfc1c
Piero Sanchi <piero.sanchi@studio.unibo.it>
2021-08-24 12:44:41 +0200

    aligned elements in statEditPanel and solved pack bug on Frame

--- src/main/scala/view/Frame.scala
+++ src/main/scala/view/Frame.scala
@@ -8,22 +8,23 @@ import javax.swing.{JFrame, JPanel, WindowConstants}
  */
 object Frame {
 
-  val minimumFrameWidth = 500
-  val minimumFrameHeight = 300
+  val minimumFrameWidth = 400
+  //val minimumFrameHeight = 300
 
   val frame = new JFrame()
   var _currentJPanel: Option[JPanel] = None
 
   def setVisible(visible: Boolean): Unit = {
     frame.setVisible(visible)
-    frame.setMinimumSize(new Dimension(minimumFrameWidth, minimumFrameHeight))
-    frame.pack()
+    frame.setMinimumSize(new Dimension(minimumFrameWidth, minimumFrameWidth))
+    //frame.pack()
     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
     frame.setTitle("ScalaQuest")
+    frame.setMinimumSize(new Dimension(frame.getWidth, frame.getHeight))
   }
 
   def setPanel(jPanel: JPanel): Unit = {
-    if(_currentJPanel.nonEmpty) frame.remove(_currentJPanel.get)
+    if (_currentJPanel.nonEmpty) frame.remove(_currentJPanel.get)
     _currentJPanel = Some(jPanel)
     frame.add(jPanel)
     frame.repaint()
