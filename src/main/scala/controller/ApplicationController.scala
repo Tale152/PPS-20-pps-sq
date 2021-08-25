8fbee5a54bb69a5f5a7e8be3793e9b5ebd74ac19
Piero Sanchi <piero.sanchi@studio.unibo.it>
2021-08-25 21:10:38 +0200

    created and integrated mainMenu in application controller

--- src/main/scala/controller/ApplicationController.scala
+++ src/main/scala/controller/ApplicationController.scala
@@ -3,12 +3,12 @@ package controller
 import controller.game.GameMasterController
 import controller.util.DirectoryInitializer.initializeGameFolderStructure
 import controller.util.ResourceName
+import controller.util.ResourceName.storyDirectoryPath
 import controller.util.serialization.ProgressSerializer
-
-import java.nio.file.{Files, Paths}
 import controller.util.serialization.StoryNodeSerializer.deserializeStory
+import view.mainMenu.MainMenu
 
-import javax.swing.JOptionPane
+import java.io.File
 
 
 /**
@@ -20,51 +20,48 @@ sealed trait ApplicationController extends Controller {
   /**
    * Load a story starting a new game. Once the story is loaded the control will be granted to the
    * StatConfigurationController in order to allow the user to choose his stats.
- *
+   *
    * @param storyUri the uri where the story that will be loaded is located
    * @see [[PlayerConfigurationController]]
    * @see [[model.characters.properties.stats.Stat]]
    */
   def loadStoryNewGame(storyUri: String): Unit
 
+  /**
+   * Resumes an already started story with previously saved data.
+   *
+   * @param storyUri the uri of the story to load
+   * @param progressUri the uri of the progress to load alongside the story
+   */
   def loadStoryWithProgress(storyUri: String, progressUri: String): Unit
+
 }
 
 object ApplicationController extends ApplicationController {
 
+  private val mainMenu: MainMenu = MainMenu(this)
+
+  private def loadStoryNames(): Set[String] = {
+    new File(storyDirectoryPath()).list().toSet
+  }
+
   initializeGameFolderStructure(ResourceName.RootGameDirectory)
 
   override def execute(): Unit = {
-    //in the future it will render the main menu
-    if(Files.exists(Paths.get(ResourceName.testRandomStoryProgressFileName()))){
-      val jopRes = JOptionPane
-        .showConfirmDialog(
-          null,
-          "Would you like to continue with your progresses?",
-          "start",
-          JOptionPane.YES_NO_OPTION
-        )
-      if(jopRes == JOptionPane.YES_OPTION){
-        loadStoryWithProgress(
-          ResourceName.testRandomStoryFileName(),
-          ResourceName.testRandomStoryProgressFileName()
-        )
-      } else {
-        loadStoryNewGame(ResourceName.testRandomStoryFileName())
-      }
-    } else {
-      loadStoryNewGame(ResourceName.testRandomStoryFileName())
-    }
+    mainMenu.setStories(loadStoryNames())
+    mainMenu.render()
   }
 
   override def close(): Unit = System.exit(0)
 
-  override def loadStoryNewGame(storyURI: String): Unit =
+  override def loadStoryNewGame(storyURI: String): Unit = {
     PlayerConfigurationController(deserializeStory(storyURI)).execute()
+  }
 
   override def loadStoryWithProgress(storyUri: String, progressUri: String): Unit = {
     GameMasterController(
       ProgressSerializer.deserializeProgress(deserializeStory(storyUri), progressUri)
     ).execute()
   }
+
 }
