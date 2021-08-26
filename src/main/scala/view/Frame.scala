e29723c8a60e0b5712ff38582688f51f26f096dd
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-26 12:21:01 +0200

    set general Frame to contain a square panel that incapsulates all view panels

--- src/main/scala/view/Frame.scala
+++ src/main/scala/view/Frame.scala
@@ -1,26 +1,71 @@
 package view
 
-import javax.swing.{JFrame, JPanel}
+import view.util.scalaQuestSwingComponents.SqSwingBorderPanel
+
+import java.awt._
+import javax.swing.border.EmptyBorder
+import javax.swing._
 
 /**
  * The one and only Frame, for all the GUI's
  */
 object Frame {
 
+  private val GameTitle: String = "ScalaQuest"
+  private val SquarePadding: Int = 10
+  private val SquareScreenPercentage: Double = 0.8
+  private val MinScreenSizePercentage: Double = 1.1
+
   val frame = new JFrame()
-  var _currentJPanel: Option[JPanel] = None
-  frame.setUndecorated(true)
-  frame.setFocusable(true)
-  frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH)
-  //frame.setMinimumSize(new Dimension(450,400))
+  private val squarePanel = new SquarePanel()
+  private val box = new Box(BoxLayout.Y_AXIS)
+
+  init()
+
+  private var _currentJPanel: Option[JPanel] = None
 
   def setVisible(visible: Boolean): Unit = frame.setVisible(visible)
 
   def setPanel(jPanel: JPanel): Unit = {
-    if (_currentJPanel.nonEmpty) frame.remove(_currentJPanel.get)
+    if (_currentJPanel.nonEmpty) squarePanel.remove(_currentJPanel.get)
     _currentJPanel = Some(jPanel)
-    frame.add(jPanel)
+    _currentJPanel.get.setBorder(new EmptyBorder(SquarePadding, SquarePadding, SquarePadding ,SquarePadding))
+    squarePanel.add(jPanel, BorderLayout.CENTER)
     frame.repaint()
+  }
+
+  private def init(): Unit = {
+    frame.setFocusable(true)
+    frame.setTitle(GameTitle)
+    frame.getContentPane.setBackground(Color.BLACK)
+    box.setAlignmentX(Component.CENTER_ALIGNMENT)
+    box.add(Box.createVerticalGlue())
+    box.add(squarePanel)
+    box.add(Box.createVerticalGlue())
+    frame.add(box)
+    frame.setMinimumSize(scaleDimension(getSquareDimension, MinScreenSizePercentage))
+    frame.pack()
+  }
+
+  private def getSquareDimension: Dimension = {
+    val screenSize = Toolkit.getDefaultToolkit.getScreenSize
+    val side: Int = (Math.min(screenSize.height, screenSize.width) * SquareScreenPercentage).toInt
+    new Dimension(side, side)
+  }
+
+  private def scaleDimension(dimension: Dimension, scale: Double): Dimension =
+    new Dimension((dimension.width * scale).toInt, (dimension.height * scale).toInt)
+
+  private class SquarePanel extends SqSwingBorderPanel {
+
+    this.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY))
+
+    override def getMinimumSize: Dimension = getSquareDimension
+
+    override def getMaximumSize: Dimension = getMinimumSize
+
+    override def getPreferredSize: Dimension = getMinimumSize
+
   }
 
 }
