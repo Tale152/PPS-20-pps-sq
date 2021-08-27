9748a089238ae66ec0f46dcf9ba6c2b105d39559
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-27 10:42:33 +0200

    styled buttons

--- src/main/scala/view/util/scalaQuestSwingComponents/SqSwingButton.scala
+++ src/main/scala/view/util/scalaQuestSwingComponents/SqSwingButton.scala
@@ -1,7 +1,8 @@
 package view.util.scalaQuestSwingComponents
 
-import java.awt.event.ActionListener
-import javax.swing.JButton
+import java.awt.Color
+import java.awt.event.{ActionListener, MouseAdapter, MouseEvent}
+import javax.swing.{BorderFactory, JButton}
 
 object SqSwingButton {
 
@@ -9,10 +10,21 @@ object SqSwingButton {
 
   class SqSwingButton(text: String, action: ActionListener, enabled: Boolean) extends JButton {
     this.setText(text)
+    this.changeAppearance(Color.WHITE)
+    this.setContentAreaFilled(false)
     this.addActionListener(action)
     this.setEnabled(enabled)
+    this.setFocusPainted(false)
+    this.setRolloverEnabled(false)
+    this.addMouseListener(new MouseAdapter {
+      override def mouseEntered(e: MouseEvent): Unit = changeAppearance(Color.GREEN)
+
+      override def mouseExited(e: MouseEvent): Unit = changeAppearance(Color.WHITE)
+    })
     this.setFocusable(false)
     this.setFont(SqFont(bold = true, btnTextSize))
+
+    private def changeAppearance(color: Color): Unit = this.setForeground(color)
   }
 
   def apply(text: String, action: ActionListener): SqSwingButton = new SqSwingButton(text, action, true)
