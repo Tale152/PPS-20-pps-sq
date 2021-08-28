5c03613255acb607211101558e51e23b5dd2cc78
Piero Sanchi <piero.sanchi@studio.unibo.it>
2021-08-28 12:52:39 +0200

    modified ControlsPanel to prevent loss of buttons, fixed hystoryview

--- src/main/scala/view/history/HistoryView.scala
+++ src/main/scala/view/history/HistoryView.scala
@@ -5,25 +5,30 @@ import view.AbstractView
 import view.history.panels.{CurrentNodePanel, PreviousChoicePanel}
 import view.util.common.ControlsPanel
 
-import javax.swing.BoxLayout
+import java.awt.{BorderLayout, GridLayout}
+import javax.swing.{BoxLayout, JPanel}
+import javax.swing.border.Border
 
 /**
  * Is a GUI that allows the user to check his previous choices traversing the story.
  * Associated with a HistoryController.
+ *
  * @see [[controller.game.subcontroller.HistoryController]]
  * @see [[model.StoryModel]]
  * @see [[model.nodes.StoryNode]]
  * @see [[model.nodes.Pathway]]
  */
-trait HistoryView extends AbstractView{
+trait HistoryView extends AbstractView {
   /**
    * Allow to set the previous choices made by player to be rendered.
+   *
    * @param choices a list of pair containing the node narrative and the chosen pathway description
    */
   def setPreviousChoices(choices: List[(String, String)]): Unit
 
   /**
    * Allow to set the current node narrative to be rendered.
+   *
    * @param narrative the current node narrative to display
    */
   def setCurrentNodeNarrative(narrative: String): Unit
@@ -35,7 +40,7 @@ object HistoryView {
 
 private class HistoryViewImpl(private val historyController: HistoryController) extends HistoryView {
 
-  this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS))
+  this.setLayout(new BorderLayout())
 
   private var _previousChoices: List[(String, String)] = List()
   private var _currentNodeDescription: String = ""
@@ -44,9 +49,13 @@ private class HistoryViewImpl(private val historyController: HistoryController) 
 
   override def setCurrentNodeNarrative(description: String): Unit = _currentNodeDescription = description
 
+  val storyRecap = new JPanel(new GridLayout(0,1))
+
   override def populateView(): Unit = {
-    _previousChoices.foreach(c => this.add(PreviousChoicePanel(c._1, c._2)))
-    this.add(CurrentNodePanel(_currentNodeDescription))
-    this.add(ControlsPanel(List(("b", ("[B] Back", _ => historyController.close())))))
+    _previousChoices.foreach(c => storyRecap.add(PreviousChoicePanel(c._1, c._2)))
+
+    this.add(storyRecap,BorderLayout.NORTH)
+    this.add(CurrentNodePanel(_currentNodeDescription), BorderLayout.CENTER)
+    this.add(ControlsPanel(List(("b", ("[B] Back", _ => historyController.close())))), BorderLayout.SOUTH)
   }
 }
