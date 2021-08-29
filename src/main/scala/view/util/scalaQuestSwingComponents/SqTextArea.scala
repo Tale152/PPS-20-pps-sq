5bbad94281b4a83ed32f23dea53e522d1f16a6ec
Tale152 <alessandro.talmi@studio.unibo.it>
2021-08-30 00:53:04 +0200

    added scrollbar in scrollable

--- src/main/scala/view/util/scalaQuestSwingComponents/SqTextArea.scala
+++ src/main/scala/view/util/scalaQuestSwingComponents/SqTextArea.scala
@@ -3,10 +3,12 @@ package view.util.scalaQuestSwingComponents
 import java.awt.Color
 import javax.swing.JTextArea
 
-case class SqTextArea(content: String) extends JTextArea(content) {
+case class SqTextArea(content: String, editable: Boolean = false) extends JTextArea(content) {
 
   private val FontSize = 15
 
+  this.setEditable(editable)
+  this.setFocusable(false)
   this.setLineWrap(true)
   this.setWrapStyleWord(true)
   this.setBackground(Color.BLACK)
