e4f3e71977e3bc69f471f6c7f8255827a2e00de0
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-09-09 18:34:10 +0200

    Added description to events and formatted related dialog

--- src/main/scala/model/nodes/Event.scala
+++ src/main/scala/model/nodes/Event.scala
@@ -6,11 +6,19 @@ import model.items.Item
 
 /**
  * Contains the strategy to pass to StoryNode's events.
+ *
  * @see [[model.nodes.StoryNode]]
  */
 sealed trait Event extends Serializable {
+
+  /**
+   * @return a Description of what happened in the event.
+   */
+  def eventDescription(): String
+
   /**
    * Strategy of what happens in a StoryNode's event.
+   *
    * @param storyModel the StoryModel to manipulate on execution.
    * @see [[model.StoryModel]]
    * @see [[model.nodes.StoryNode]]
@@ -20,18 +28,20 @@ sealed trait Event extends Serializable {
 
 /**
  * [[model.nodes.Event]] that contains a [[model.characters.properties.stats.StatModifier]].
+ *
  * @param statModifier the [[model.characters.properties.stats.StatModifier]] contained in the event.
  */
-case class StatEvent (statModifier: StatModifier) extends Event {
+case class StatEvent(override val eventDescription: String, statModifier: StatModifier) extends Event {
   override def handle(storyModel: StoryModel): Unit =
     storyModel.player.properties.statModifiers += statModifier
 }
 
 /**
  * [[model.nodes.Event]] that contains a [[model.items.Item]].
+ *
  * @param item the [[model.items.Item]] contained in the event.
  */
-case class ItemEvent(item: Item) extends Event {
+case class ItemEvent(override val eventDescription: String, item: Item) extends Event {
   override def handle(storyModel: StoryModel): Unit =
     storyModel.player.inventory = storyModel.player.inventory :+ item
 }
--- src/main/scala/model/nodes/util/RandomStoryNodeGenerator.scala
+++ src/main/scala/model/nodes/util/RandomStoryNodeGenerator.scala
@@ -23,27 +23,82 @@ object RandomStoryNodeGenerator {
 
   import model.nodes.util.RandomStoryNodeGenerator.RandomStoryParams._
 
-  private def rnd(max: Int): Int = Random.nextInt(max) + 1
+  object Generator {
 
-  private def getMaxId(nodes: Seq[StoryNode]): Int = nodes.map(n => n.id).max
+    private object Utility {
+      def rnd(max: Int): Int = Random.nextInt(max) + 1
 
-  def generate(): StoryNode = {
-    val generateLastLayer = () =>
-      for (x <- 0 until rnd(MaxNodesInLayer)) yield StoryNode(x, "final node " + x, None, Set.empty, List())
+      def getMaxId(nodes: Seq[StoryNode]): Int = nodes.map(n => n.id).max
 
-    def generateLayers(depth: Int): Seq[StoryNode] = depth match {
+      def setEnemy(): Option[Enemy] = {
+
+        val statValue: Int = 5
+        val maxPossibleHealth: Int = 100
+        val stats: Set[Stat] = Set(
+          Stat(statValue, StatName.Strength),
+          Stat(statValue, StatName.Constitution),
+          Stat(statValue, StatName.Wisdom),
+          Stat(statValue, StatName.Charisma),
+          Stat(statValue, StatName.Intelligence),
+          Stat(statValue, StatName.Dexterity)
+        )
+        rnd(EnemyProbability) match {
+          case 1 => Some(Enemy("Giorno Giovanna", rnd(maxPossibleHealth), stats))
+          case _ => None
+        }
+      }
+
+      def setEvents(): List[Event] = {
+        var res: List[Event] = List()
+        if (rnd(EventProbability) == 1) {
+          res = ItemEvent(
+            "This is an item event, it is used to notify the user that an item was found.",
+            randomItem()
+          ) :: res
+        }
+        if (rnd(EventProbability) == 1) {
+          res = StatEvent(
+            "This is a stat event, it is used to notify the user that some stats have changed.",
+            StatModifier(StatName.Strength, i => i + 5)
+          ) :: res
+        }
+        res
+      }
+
+      def randomItem(): Item = {
+        rnd(3) match {
+          case 1 => KeyItem("key-item", "Key items can't be used but are useful during the story.")
+          case 2 => ConsumableItem(
+            "consumable-item",
+            "Consumable items will disappear after use.",
+            c => c.properties.health.currentPS += 10
+          )
+          case 3 => EquipItem(
+            "equip-item",
+            "Equip items can be equipped and will boost your stats.",
+            Set(StatModifier(StatName.Strength, v => v + 1)),
+            EquipItemType.Weapon
+          )
+        }
+      }
+    }
+
+    import model.nodes.util.RandomStoryNodeGenerator.Generator.Utility._
+
+    private def generateLayers(depth: Int): Seq[StoryNode] = depth match {
       case 0 => generateLastLayer()
       case _ => generateIntermediateLayer(depth)
     }
 
-    def genPathwaysToNextLayerNode(node: StoryNode, nCurrentLayersNodes: Int): MutableSet[Pathway] =
+    private def genPathwaysToNextLayerNode(node: StoryNode, nCurrentLayersNodes: Int): MutableSet[Pathway] =
       (for (_ <- 0 until Random.nextInt(nCurrentLayersNodes) + 1)
         yield Pathway("go to node " + node.id, node, None)).toSet.to[MutableSet]
 
-    def genPathwaysToAllNextLayerNodes(nextLayer: Seq[StoryNode], nCurrentLayerNodes: Int): Seq[MutableSet[Pathway]] =
+    private def genPathwaysToAllNextLayerNodes(nextLayer: Seq[StoryNode],
+                                               nCurrentLayerNodes: Int): Seq[MutableSet[Pathway]] =
       for (node <- nextLayer) yield genPathwaysToNextLayerNode(node, nCurrentLayerNodes)
 
-    def generateIntermediateLayer(depth: Int): Seq[StoryNode] = {
+    private def generateIntermediateLayer(depth: Int): Seq[StoryNode] = {
       val nextLayer = generateLayers(depth - 1)
       val startingId = getMaxId(nextLayer) + 1
       val nCurrentLayerNodes = rnd(MaxNodesInLayer)
@@ -61,65 +116,23 @@ object RandomStoryNodeGenerator {
         val narrative =
           if (newNodePathways.isEmpty) "final node " + id else "node " + id + ", max remaining layers " + depth
 
-        res = res :+ StoryNode(id, narrative, setEnemy(), newNodePathways.toSet, List(ItemEvent(randomItem())))
+        res = res :+ StoryNode(id, narrative, setEnemy(), newNodePathways.toSet, setEvents())
       }
       res
     }
 
-    val generated = generateLayers(Layers - 1)
-    val pathways: Seq[Pathway] = for (node <- generated) yield Pathway("go to node " + node.id, node, None)
+    private def generateLastLayer(): IndexedSeq[StoryNode] =
+      for (x <- 0 until rnd(MaxNodesInLayer)) yield StoryNode(x, "final node " + x, None, Set.empty, List())
 
-    StoryNode(getMaxId(generated) + 1,
-      "starting node, max remaining layers " + Layers,
-      None,
-      pathways.toSet,
-      setEvents())
-  }
+    def generate(): StoryNode = {
+      val generated = generateLayers(Layers - 1)
+      val pathways: Seq[Pathway] = for (node <- generated) yield Pathway("go to node " + node.id, node, None)
 
-  private def setEnemy(): Option[Enemy] = {
-
-    val statValue: Int = 5
-    val maxPossibleHealth: Int = 100
-    val stats: Set[Stat] =  Set(
-      Stat(statValue, StatName.Strength),
-      Stat(statValue, StatName.Constitution),
-      Stat(statValue, StatName.Wisdom),
-      Stat(statValue, StatName.Charisma),
-      Stat(statValue, StatName.Intelligence),
-      Stat(statValue, StatName.Dexterity)
-    )
-    rnd(EnemyProbability) match {
-      case 1 => Some(Enemy("Giorno Giovanna", rnd(maxPossibleHealth), stats))
-      case _ => None
+      StoryNode(getMaxId(generated) + 1,
+        "starting node, max remaining layers " + Layers,
+        None,
+        pathways.toSet,
+        setEvents())
     }
   }
-
-  private def setEvents(): List[Event] = {
-    var res: List[Event] = List()
-    if(rnd(EventProbability) == 1){
-      res = ItemEvent(KeyItem("sword", "it's a sword")) :: res
-    }
-    if(rnd(EventProbability) == 1){
-      res = StatEvent(StatModifier(StatName.Strength, i => i + 5)) :: res
-    }
-    res
-  }
-
-  private def randomItem(): Item = {
-    rnd(3) match {
-      case 1 => KeyItem("key-item", "Key items can't be used but are useful during the story.")
-      case 2 => ConsumableItem(
-        "consumable-item",
-        "Consumable items will disappear after use.",
-        c=> c.properties.health.currentPS += 10
-      )
-      case 3 => EquipItem(
-        "equip-item",
-        "Equip items can be equipped and will boost your stats.",
-        Set(StatModifier(StatName.Strength, v => v + 1)),
-        EquipItemType.Weapon
-      )
-    }
-  }
-
 }
