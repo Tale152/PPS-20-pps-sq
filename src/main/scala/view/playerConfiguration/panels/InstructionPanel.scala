476aefe87f1c10e53ac4db472e9b47470ef17e92
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-09-10 15:37:46 +0200

    InstrucionPanel (playerConfiguration) is now a case class

--- src/main/scala/view/playerConfiguration/panels/InstructionPanel.scala
+++ src/main/scala/view/playerConfiguration/panels/InstructionPanel.scala
@@ -2,19 +2,10 @@ package view.playerConfiguration.panels
 
 import view.util.scalaQuestSwingComponents.{SqSwingFlowPanel, SqSwingLabel}
 
-object InstructionPanel {
-
-  private object InstructionCommons {
-    val TextSize = 17
-  }
-
-  /**
-   * Panel contained in [[view.playerConfiguration.PlayerConfigurationView]]; renders instructions for the player
-   */
-  class InstructionPanel() extends SqSwingFlowPanel {
-    import view.playerConfiguration.panels.InstructionPanel.InstructionCommons.TextSize
-    this.add(SqSwingLabel("Distribute points to set your character's stats", bold = true, labelSize = TextSize))
-  }
-
-  def apply(): InstructionPanel = new InstructionPanel()
+/**
+ * Panel contained in [[view.playerConfiguration.PlayerConfigurationView]]; renders instructions for the player
+ */
+case class InstructionPanel() extends SqSwingFlowPanel {
+  private val textSize = 17
+  this.add(SqSwingLabel("Distribute points to set your character's stats", bold = true, labelSize = textSize))
 }
