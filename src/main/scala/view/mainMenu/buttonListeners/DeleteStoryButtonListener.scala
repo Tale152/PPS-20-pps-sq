ce2df865242ba71fbff86e28b104adc77206d9f9
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-09-10 15:51:12 +0200

    Changed MainMenu form field name to be more general and understandable

--- src/main/scala/view/mainMenu/buttonListeners/DeleteStoryButtonListener.scala
+++ src/main/scala/view/mainMenu/buttonListeners/DeleteStoryButtonListener.scala
@@ -3,8 +3,10 @@ package view.mainMenu.buttonListeners
 import controller.ApplicationController
 import controller.util.Resources.ResourceName.MainDirectory.RootGameDirectory
 import controller.util.Resources.ResourceName.storyDirectoryPath
+import view.form.{Form, FormBuilder}
 import view.mainMenu.buttonListeners.MainMenuButtonListeners.MainMenuButtonListener
 import view.mainMenu.forms.DeleteStory.showDeleteStoryForm
+import view.mainMenu.forms.DeleteStoryConfirmListener
 
 import java.awt.event.ActionEvent
 import java.io.File
@@ -20,6 +22,14 @@ case class DeleteStoryButtonListener(override val applicationController: Applica
   override def actionPerformed(e: ActionEvent): Unit = {
     showDeleteStoryForm(applicationController,
       new File(storyDirectoryPath(RootGameDirectory)).listFiles().toList.map(f => f.getName))
+  }
+
+  private def showDeleteStoryForm(applicationController: ApplicationController, comboElements: List[String]): Unit = {
+    val form: Form = FormBuilder()
+      .addComboField("Select the story to delete:", comboElements)
+      .get(applicationController)
+    form.setOkButtonListener(DeleteStoryConfirmListener(form, applicationController, comboElements))
+    form.render()
   }
 
 }
