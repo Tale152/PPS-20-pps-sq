ad705eac0d44d7b554cd22644d4e918bbbf2e978
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-09-13 19:53:02 +0200

    Added object for prolog predicates and records name

---
+++ src/main/scala/controller/prolog/PrologNames.scala
@@ -0,0 +1,12 @@
+package controller.prolog
+
+object PrologNames {
+
+  object Predicates {
+    val StoryNodePredicate: String = "story_node"
+  }
+
+  object Records {
+    val PathwayRecord: String = "pathway"
+  }
+}
