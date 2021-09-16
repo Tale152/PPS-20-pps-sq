217646a9ffd7c6b7da38ac20e1c37c85ae8af71a
Tale152 <alessandro.talmi@studio.unibo.it>
2021-09-16 20:28:59 +0200

    refactored and vincoled more new enemy feature

--- src/main/scala/view/editor/forms/enemies/NewEnemy.scala
+++ src/main/scala/view/editor/forms/enemies/NewEnemy.scala
@@ -1,6 +1,7 @@
 package view.editor.forms.enemies
 
 import controller.editor.EditorController
+import view.editor.EditorView
 import view.editor.okButtonListener.enemies.NewEnemyOkListener
 import view.form.{Form, FormBuilder}
 
@@ -8,22 +9,35 @@ object NewEnemy {
 
   private val DefaultStatValue: Integer = 10
 
+  val NodeIdIndex = 0
+  val EnemyNameIndex = 1
+  val EnemyHealthIndex = 2
+  val CharismaIndex = 3
+  val ConstitutionIndex = 4
+  val DexterityIndex = 5
+  val IntelligenceIndex = 6
+  val StrengthIndex = 7
+  val WisdomIndex = 8
+
   def showNewEnemyForm(editorController: EditorController): Unit = {
-    val form: Form = FormBuilder()
-      .addComboField(
-        "In which node you want to want to insert an enemy? (id)",
-        editorController.getNodesIds(n => n.enemy.isEmpty).map(n => n.toString)
-      )
-      .addTextField("Enemy name")
-      .addSpinnerNumberField("Health", DefaultStatValue)
-      .addSpinnerNumberField("Charisma", DefaultStatValue)
-      .addSpinnerNumberField("Constitution", DefaultStatValue)
-      .addSpinnerNumberField("Dexterity", DefaultStatValue)
-      .addSpinnerNumberField("Intelligence", DefaultStatValue)
-      .addSpinnerNumberField("Strength", DefaultStatValue)
-      .addSpinnerNumberField("Wisdom", DefaultStatValue)
-      .get(editorController)
-    form.setOkButtonListener(NewEnemyOkListener(form, editorController))
-    form.render()
+    val targetNodes = editorController.getNodesIds(n => n.enemy.isEmpty).map(n => n.toString)
+    if(targetNodes.nonEmpty){
+      val form: Form = FormBuilder()
+        .addComboField("In which node you want to want to insert an enemy? (id)",targetNodes)
+        .addTextField("Enemy name")
+        .addSpinnerNumberField("Health", DefaultStatValue)
+        .addSpinnerNumberField("Charisma", DefaultStatValue)
+        .addSpinnerNumberField("Constitution", DefaultStatValue)
+        .addSpinnerNumberField("Dexterity", DefaultStatValue)
+        .addSpinnerNumberField("Intelligence", DefaultStatValue)
+        .addSpinnerNumberField("Strength", DefaultStatValue)
+        .addSpinnerNumberField("Wisdom", DefaultStatValue)
+        .get(editorController)
+      form.setOkButtonListener(NewEnemyOkListener(form, editorController))
+      form.render()
+    } else {
+      EditorView.showForbiddenActionDialog("There are no nodes without an enemy")
+    }
+
   }
 }
--- src/main/scala/view/editor/okButtonListener/enemies/NewEnemyOkListener.scala
+++ src/main/scala/view/editor/okButtonListener/enemies/NewEnemyOkListener.scala
@@ -6,8 +6,8 @@ import model.characters.properties.stats.{Stat, StatName}
 import view.editor.EditorConditionValues.ConditionDescriptions.Subjects.{TheId, TheName}
 import view.editor.EditorConditionValues.ConditionDescriptions.mustBeSpecified
 import view.editor.EditorConditionValues.InputPredicates.NonEmptyString
+import view.editor.forms.enemies.NewEnemy._
 import view.editor.okButtonListener.EditorOkFormButtonListener
-import view.editor.okButtonListener.enemies.NewEnemyOkListenerConst._
 import view.form.Form
 
 case class NewEnemyOkListener(override val form: Form,
@@ -31,24 +31,9 @@ case class NewEnemyOkListener(override val form: Form,
   )
 
   override def inputConditions: List[(Boolean, String)] = List(
-    (form.elements(NodeIdIndex).value != null, mustBeSpecified(TheId)),
     (NonEmptyString(form.elements(EnemyNameIndex).value), mustBeSpecified(TheName))
   )
 
   override def stateConditions: List[(Boolean, String)] = List()
-
-}
-
-private object NewEnemyOkListenerConst {
-
-  val NodeIdIndex = 0
-  val EnemyNameIndex = 1
-  val EnemyHealthIndex = 2
-  val CharismaIndex = 3
-  val ConstitutionIndex = 4
-  val DexterityIndex = 5
-  val IntelligenceIndex = 6
-  val StrengthIndex = 7
-  val WisdomIndex = 8
 
 }
