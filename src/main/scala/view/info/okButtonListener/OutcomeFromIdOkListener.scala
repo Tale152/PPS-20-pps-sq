181da7dfdd7b0a7dcd19f662ce2cc6ec57e34ebb
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-09-17 16:38:23 +0200

    Added file save for possible outcome from id

--- src/main/scala/view/info/okButtonListener/OutcomeFromIdOkListener.scala
+++ src/main/scala/view/info/okButtonListener/OutcomeFromIdOkListener.scala
@@ -1,10 +1,15 @@
 package view.info.okButtonListener
 
 import controller.InfoController
+import controller.prolog.structs.StructsNames.Predicates.ReachAllFinalNodesPredicateName
+import controller.util.Resources.ResourceName.FileExtensions.TxtExtension
+import controller.util.serialization.FileUtil.TextFileBuilder
+import controller.util.serialization.StringUtil.listFormattedLikeArray
 import view.editor.EditorConditionValues.ConditionDescriptions.Subjects.TheStartingId
 import view.editor.EditorConditionValues.ConditionDescriptions.mustBeSpecified
 import view.editor.EditorConditionValues.InputPredicates.NonEmptyString
 import view.form.Form
+import view.util.scalaQuestSwingComponents.dialog.SqOkSwingDialog
 
 case class OutcomeFromIdOkListener(override val form: Form, override val controller: InfoController)
   extends InfoOkFormButtonListener(form, controller) {
@@ -14,7 +19,22 @@ case class OutcomeFromIdOkListener(override val form: Form, override val control
 
   override def stateConditions: List[(Boolean, String)] = List()
 
-  override def performActionCondition: () => Boolean = ???
+  override def performActionCondition: () => Boolean =
+    () => controller.storyOutcomes(form.elements.head.value.toInt).nonEmpty
 
-  override def saveFile(folderPath: String): Unit = ???
+  override def saveFile(folderPath: String): Unit = {
+    val startId = form.elements.head.value.toInt
+    val solutions = controller.storyOutcomes(form.elements.head.value.toInt)
+    val filePath: String = folderPath + "/" + ReachAllFinalNodesPredicateName + "(" + startId + ",X)." + TxtExtension
+    val builder = TextFileBuilder()
+    builder.addText("All the possible outcome from node " + startId + "\n\n")
+    solutions.foreach(s => builder.addText(listFormattedLikeArray(s) + "\n"))
+    builder.outputFile(filePath)
+    SqOkSwingDialog(
+      "File created",
+      "File created in path" + filePath,
+      _ => controller.execute()
+    )
+  }
+
 }
