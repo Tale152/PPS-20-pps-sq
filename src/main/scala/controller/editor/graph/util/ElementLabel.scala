c554049ae0c75e9e7c3463adc2ec716a73ad4418
Tale152 <alessandro.talmi@studio.unibo.it>
2021-09-23 16:13:01 +0200

    commented ElementLabel object

--- src/main/scala/controller/editor/graph/util/ElementLabel.scala
+++ src/main/scala/controller/editor/graph/util/ElementLabel.scala
@@ -2,15 +2,24 @@ package controller.editor.graph.util
 
 import org.graphstream.graph.Element
 
+/**
+ * Utility object used to put a label on a GraphStream's Graph element.
+ */
 object ElementLabel {
 
   private val LabelAttribute: String = "ui.label"
 
+  /**
+   * Used to put a label on a GraphStream's Graph element.
+   * @param element the GraphStream's Graph element to put a label on
+   * @param flag Boolean used to determine which string to use as the label
+   * @param onTrue String that will be used as label if flag is true
+   * @param onFalse String that will be used as label if flag is false
+   */
   def putLabelOnElement(element: Element, flag: Boolean)(onTrue: String, onFalse: String): Unit = {
     if (flag) {
       element.setAttribute(LabelAttribute, onTrue)
-    }
-    else {
+    } else {
       element.setAttribute(LabelAttribute, onFalse)
     }
   }
