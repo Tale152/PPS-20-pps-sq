d4c44447b3d89cea1d86e3669bed175ed18a978d
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-09-24 18:34:10 +0200

    Fixed bug that caused the application to crash if the path where the jar is located contains a space or other characters that needs encoding

--- src/main/scala/controller/util/Resources.scala
+++ src/main/scala/controller/util/Resources.scala
@@ -7,6 +7,7 @@ import controller.util.Resources.ResourceName.{interactionSoundEffectPath, navig
 import java.awt.image.BufferedImage
 import java.io.{BufferedInputStream, File, InputStream}
 import java.net.{URI, URL}
+import java.nio.charset.StandardCharsets
 import java.nio.file._
 import javax.imageio.ImageIO
 import javax.sound.sampled.{AudioInputStream, AudioSystem, Clip}
@@ -20,6 +21,7 @@ object Resources {
 
   /**
    * Implicit class used to add a method to map a [[java.io.InputStream]] to a [[javax.sound.sampled.AudioInputStream]].
+   *
    * @param inputStream the implicit InputStream parameter.
    */
   implicit class RichInputStream(inputStream: InputStream) {
@@ -44,7 +46,7 @@ object Resources {
    * @param folderPath the folder path.
    * @return A set of (FileName, [[java.io.InputStream]]).
    */
-  def resourcesAsNamedInputStreamFromFolder(folderPath: String): Set[(String,InputStream)] = {
+  def resourcesAsNamedInputStreamFromFolder(folderPath: String): Set[(String, InputStream)] = {
     val url: Option[URL] = Option(getClass.getResource(folderPath))
     url match {
       case Some(url) =>
@@ -75,11 +77,14 @@ object Resources {
    * @return A set of (FileName, [[java.io.InputStream]])
    */
   private def resourceAsInputStreamFromJarFolder(folderPath: String): Set[(String, InputStream)] = {
-    val jarFile: Path = Paths.get(
-      getClass.getProtectionDomain.getCodeSource.getLocation.toString
-        .substring("file:".length()).replaceFirst("^/(.:/)", "$1")
-    )
-    val fs: FileSystem = FileSystems.newFileSystem(jarFile, getClass.getClassLoader)
+    val decodedFileSystemURL: String = {
+      java.net.URLDecoder.decode(
+        getClass.getProtectionDomain.getCodeSource.getLocation.toString.substring("file:".length())
+          .replaceFirst("^/(.:/)", "$1"),
+        StandardCharsets.UTF_8.name
+      )
+    }
+    val fs = FileSystems.newFileSystem(Paths.get(decodedFileSystemURL), getClass.getClassLoader)
     Files.newDirectoryStream(fs.getPath(folderPath))
       .iterator()
       .asScala
