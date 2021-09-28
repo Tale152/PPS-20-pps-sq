54aa65289ecae6fa7e8e166fa79ae2288d3920ce
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-09-28 11:31:28 +0200

    toMutable and toImmutable are now methods in implicit classes

--- src/main/scala/controller/editor/StoryNodeConverter.scala
+++ src/main/scala/controller/editor/StoryNodeConverter.scala
@@ -1,50 +1,64 @@
 package controller.editor
 
-import controller.editor.StoryNodeConverterType.{ConversionType, Immutable, Mutable}
+import controller.editor.StoryNodeConverter.StoryNodeConverterType.{ConversionType, Immutable, Mutable}
 import model.nodes.StoryNode.MutableStoryNode
 import model.nodes.{MutablePathway, Pathway, StoryNode}
 
-object StoryNodeConverterType {
-
-  sealed trait ConversionType
-
-  case object Mutable extends ConversionType
-
-  case object Immutable extends ConversionType
-}
-
-case
 /**
  * Utility object used to convert stories that use StoryNodes and Pathways
  * to stories that use MutableStoryNodes and MutablePathways and the other way around.
  */
 object StoryNodeConverter {
 
-  /**
-   * Taking a StoryNode as a route node, recreates the same nodes structures using MutableStoryNodes.
-   * @param routeNode the StoryNode that is the route of a story
-   * @return a pair containing the new mutable route node and a set with all mutable nodes composing the story
-   */
-  def fromImmutableToMutable(routeNode: StoryNode): (MutableStoryNode, Set[MutableStoryNode]) = {
-    val t = traverse(routeNode, Set(), Mutable)
-    (t.filter(n => n.id == routeNode.id).last.asInstanceOf[MutableStoryNode], t.asInstanceOf[Set[MutableStoryNode]])
+  object StoryNodeConverterType {
+
+    sealed trait ConversionType
+
+    case object Mutable extends ConversionType
+
+    case object Immutable extends ConversionType
+
   }
 
   /**
-   * Taking a MutableStoryNode as a route node, recreates the same nodes structures using
-   * StoryNodes that aren't mutable.
-   * @param routeNode the MutableStoryNode that is the route of a story
-   * @return a pair containing the new immutable route node and a set with all immutable nodes composing the story
+   * Implicit class used to do conversion operation of an Immutable [[model.nodes.StoryNode]].
+   *
+   * @param routeNode the [[model.nodes.StoryNode]]
    */
-  def fromMutableToImmutable(routeNode: MutableStoryNode): (StoryNode, Set[StoryNode]) = {
-    val t = traverse(routeNode, Set(), Immutable)
-    (t.filter(n => n.id == routeNode.id).last, t)
+  implicit class ImmutableStoryNodeConverter(routeNode: StoryNode) {
+    /**
+     * Taking a StoryNode as a route node, recreates the same nodes structures using MutableStoryNodes.
+     *
+     * @return a pair containing the new mutable route node and a set with all mutable nodes composing the story
+     */
+    def toMutable: (MutableStoryNode, Set[MutableStoryNode]) = {
+      val t = traverse(routeNode, Set(), Mutable)
+      (t.filter(n => n.id == routeNode.id).last.asInstanceOf[MutableStoryNode], t.asInstanceOf[Set[MutableStoryNode]])
+    }
+  }
+
+  /**
+   * Implicit class used to do conversion operation of a [[model.nodes.StoryNode]].
+   *
+   * @param routeNode the MutableStoryNode that is the route of a story
+   */
+  implicit class MutableStoryNodeConverter(routeNode: MutableStoryNode) {
+    /**
+     * Taking a MutableStoryNode as a route node, recreates the same nodes structures using
+     * StoryNodes that aren't mutable.
+     *
+     * @return a pair containing the new immutable route node and a set with all immutable nodes composing the story
+     */
+    def toImmutable: (StoryNode, Set[StoryNode]) = {
+      val t = traverse(routeNode, Set(), Immutable)
+      (t.filter(n => n.id == routeNode.id).last, t)
+    }
   }
 
   private def traverse(node: StoryNode, traversed: Set[StoryNode], conversionType: ConversionType): Set[StoryNode] = {
     var t = traversed
-    val pathways = for(p <- node.pathways) yield {
-      if(!t.exists(n => n.id == p.destinationNode.id)){
+    val pathways = for (p <- node.pathways) yield {
+      if (!t.exists(n => n.id == p.destinationNode.id)) {
         t ++= traverse(p.destinationNode, t, conversionType)
       }
       instantiatePathway(p, t, conversionType)
