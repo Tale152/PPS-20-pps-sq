b4a1be15d043e7fc15e8ecea4f970c0d68f13cfc
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2021-09-29 10:49:14 +0200

    Fixed bug that allows to end the player life using an item

--- src/main/scala/view/inventory/InventoryView.scala
+++ src/main/scala/view/inventory/InventoryView.scala
@@ -9,7 +9,8 @@ import view.AbstractView
 import view.inventory.panels.inventoryPanel
 import view.util.StringUtil.TitleSize
 import view.util.common.ControlsPanel
-import view.util.scalaQuestSwingComponents.SqSwingLabel
+import view.util.scalaQuestSwingComponents.{SqSwingButton, SqSwingLabel}
+import view.util.scalaQuestSwingComponents.dialog.SqSwingDialog
 
 /**
  * A GUI that allows the user to view, use and discard the items in his possession.
@@ -23,6 +24,10 @@ sealed trait InventoryView extends AbstractView {
    */
   def setItems(items: List[Item]): Unit
 
+  /**
+   * Shows a dialog that inform the player that he suicide himself using an item.
+   */
+  def setSuicide(): Unit
 }
 
 object InventoryView {
@@ -44,6 +49,13 @@ object InventoryView {
         BorderLayout.NORTH)
       this.add(inventoryPanel.InventoryPanel(inventoryController, _inventoryItems), BorderLayout.CENTER)
       this.add(ControlsPanel(List(("b", ("[B] Back", _ => inventoryController.close())))), BorderLayout.SOUTH)
+    }
+
+    override def setSuicide(): Unit = {
+      SqSwingDialog("Oh no... You committed suicide!" ,
+        "You used an item that ended your life... Be more careful next time...",
+        List(SqSwingButton("ok", _ => inventoryController.close())),
+        closable = false)
     }
 
   }
