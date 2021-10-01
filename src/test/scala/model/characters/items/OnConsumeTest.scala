42593f31247dac2a3095781bb5ec6c05d9abf8cd
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-10-01 17:22:27 +0200

    Added OnConsumeTest

---
+++ src/test/scala/model/characters/items/OnConsumeTest.scala
@@ -0,0 +1,27 @@
+package model.characters.items
+
+import mock.MockFactory.CharacterFactory
+import model.characters.Player
+import model.items.{DecrementHealthOnConsume, IncrementHealthOnConsume}
+import specs.{FlatTestSpec, SerializableSpec}
+
+class OnConsumeTest extends FlatTestSpec with SerializableSpec {
+
+  val playerPS: Int = 100
+  val player: Player = Player("jojo", playerPS, CharacterFactory.mockSetOfStats())
+
+  "The DecrementHealthOnConsume" should "work properly" in {
+    val decrementValue: Int = 50
+    val decrementHealthOnConsume = DecrementHealthOnConsume(decrementValue)
+    decrementHealthOnConsume(player)
+    player.properties.health.currentPS shouldEqual 50
+  }
+
+  "The IncrementHealthOnConsume" should "work properly" in {
+    val incrementValue: Int = 10
+    val incrementHealthOnConsume = IncrementHealthOnConsume(incrementValue)
+    incrementHealthOnConsume(player)
+    player.properties.health.currentPS shouldEqual 60
+  }
+
+}
