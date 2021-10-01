83ec9e8c4f8b6b345abcd5398ee63a218524d682
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-10-01 17:30:56 +0200

    Added docs to OnConsume and OnApplyStatModifier

--- src/main/scala/model/items/OnConsume.scala
+++ src/main/scala/model/items/OnConsume.scala
@@ -2,13 +2,24 @@ package model.items
 
 import model.characters.Character
 
+/**
+ * Trait that represents the strategy to apply on a [[model.items.ConsumableItem]].
+ */
 sealed trait OnConsume extends (Character => Unit)
 
+/**
+ * Increment the [[model.characters.Character]] current PS by some value.
+ * @param value the value to add to the current PS of the character.
+ */
 case class IncrementHealthOnConsume(value: Int) extends OnConsume {
   override def apply(character: Character): Unit =
     character.properties.health.currentPS = character.properties.health.currentPS + value
 }
 
+/**
+ * Decrement the [[model.characters.Character]] current PS by some value.
+ * @param value the value to subtract from the current PS of the character.
+ */
 case class DecrementHealthOnConsume(value: Int) extends OnConsume {
   override def apply(character: Character): Unit =
     character.properties.health.currentPS = character.properties.health.currentPS - value
